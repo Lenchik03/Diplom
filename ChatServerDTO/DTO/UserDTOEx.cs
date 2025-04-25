5d0cd8581ea9b1186498fe7f81ad492c79576a1c
Lenchik <aliona.nikitina.01@gmail.com>
2025-04-25 11:19:24 +1000

    eewfewf

--- ChatServerDTO/DTO/UserDTOEx.cs
+++ ChatServerDTO/DTO/UserDTOEx.cs
@@ -9,5 +9,6 @@ namespace ProjectSystemAPI.DTO
     public partial class UserDTO
     {
         public string FIO { get => $"{FirstName} {LastName} {Patronymic}"; }
+        public string Initials { get => $"{LastName} {FirstName[0]}. {Patronymic[0]}."; }
     }
 }
